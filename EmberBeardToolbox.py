fdd6f0778c162371967c777e758a1ac9ede30965
EmberBeard <emberbeard42@gmail.com>
2025-07-13 18:29:20 +0100

    Implemented the bind function
    Implemented the binding function for making a game armature mirror a control rig EXACTLY. I then modified the unbind so folks could bind and unbind without deselecting any objects. It all seems to work great. I'll get an example blender scene that I've been using to test uploaded in a sec as a proof of work/demo. I might take some time to polish it up a bit actually... we'll see... suppose I could also iterate on it?

--- EmberBeardToolbox.py
+++ EmberBeardToolbox.py
@@ -86,6 +86,39 @@ class ARM_OT_BindControlRig(bpy.types.Operator):
     
     def execute(self, context):
         print("Binding the control rig")
+        Selection = context.selected_objects
+        if(len(Selection) != 2):
+            print("Must have only two armature objects selected")
+            return {"CANCELLED"}
+        if(context.selected_objects[0].type != 'ARMATURE' or context.selected_objects[1].type != 'ARMATURE'):
+            print("Must have only two armature objects selected")
+            return {"CANCELLED"}
+        
+        armature_from = None
+        armature_to = None
+
+        if(context.active_object == context.selected_objects[0]):
+            armature_to = context.selected_objects[0]
+            armature_from = context.selected_objects[1]
+        else:
+            armature_from = context.selected_objects[0]
+            armature_to = context.selected_objects[1]
+        
+        current_mode = context.mode
+        bpy.ops.object.mode_set(mode='POSE')
+
+        for t_bone in armature_to.pose.bones:
+            for f_bone in armature_from.pose.bones:
+                if (t_bone.name == f_bone.name):
+                    copy_con = t_bone.constraints.new('COPY_TRANSFORMS')
+                    copy_con.target = armature_from
+                    copy_con.subtarget = f_bone.name
+                    copy_con.target_space = 'WORLD' # consider POSE
+                    copy_con.owner_space = 'WORLD' # consider POSE
+                    break
+
+        bpy.ops.object.mode_set(mode=current_mode)
+
         return {"FINISHED"}
 
 # Remove Control Rig operator
@@ -99,11 +132,11 @@ class ARM_OT_RemoveControlRig(bpy.types.Operator):
     def execute(self, context):
         print("Removing the control rig")
         Selection = context.selected_objects
-        if(len(Selection) != 1):
-            print("Must have only one armature object selected")
+        if(len(Selection) == 0):
+            print("Must have one armature object as the active selection")
             return {"CANCELLED"}
         if(context.active_object.type != 'ARMATURE'):
-            print("Must have only one armature object selected")
+            print("Must have one armature object as the active selection")
             return {"CANCELLED"}
         
         armature = context.active_object
@@ -135,7 +168,7 @@ class CON_OT_ClearConsole(bpy.types.Operator):
         
         return {"FINISHED"}
 
-#============================pip install fake-bpy-module-latest===============================
+#===========================================================
 # The main helper panel with all the UI
 #===========================================================
 
